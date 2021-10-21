7fe1885a354feac8a8017cdce2e9ce993d58dac4
Alin Manole <alin.manole@yahoo.com>
2021-10-21 16:59:39 +0200

    5pm

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// 01 
+// 01 - Push sans push
 
 let fruits = ['Apple','Orange','Banana']
 fruits = [...fruits, 'Raspberry'] 
 
-// 02 
+// 02 - Unshift sans unshift 
 
 console.log(fruits)
 
@@ -14,7 +14,7 @@ fruits = [
 
 console.log(fruits)
 
-// 03 
+// 03 - Array concatenation
 
 const arr1 = ['A', 'B', 'C']
 const arr2 = ['X', 'Y', 'Z']
@@ -23,4 +23,29 @@ const arr3 = arr1.concat(arr2);
 
 console.log(arr3)
 
-// 04
+// 04 - Objects
+
+const user = {
+    age: 10,
+    eyes: "vert",
+    legs: 2,
+    arms: 2,
+    hair: "long",
+    caracterstique: "gras du bide",
+    nose: "petit nez",
+    ears: "grandes oreilles",
+    glasses: true,
+    sosie: "sarkozy"
+  }
+
+  const bobby = {
+    firstName: "Bobby",
+	...user	
+}
+
+console.log(bobby)
+
+
+// 05 - forEach
+
+    const numbers = [1, 2, 3, 4, 5, 6]
